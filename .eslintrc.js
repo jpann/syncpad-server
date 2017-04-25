7622624a83c917bf5b31d5e10a6bdd62815c80b2
jpann <ikonmobile@gmail.com>
2017-04-25 11:38:37 -0600

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-undef": "warn",
+        "no-unused-vars": "warn",
+    }
+};
+
