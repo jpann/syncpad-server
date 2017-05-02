41b52ca284b5878362b4cdfc9a6b85d711c470b2
jpann <ikonmobile@gmail.com>
2017-05-02 14:01:23 -0600

    Added navbar

--- httpServer.js
+++ httpServer.js
@@ -39,8 +39,16 @@ passport.use(new Strategy(
     {
         database.validate_user(username, password, function(err, user)
         {
-            if (err) { return cb(err); }
-            if (!user) { return cb(null, false); }
+            if (err) 
+            { 
+                return cb(err); 
+            }
+
+            if (!user) 
+            { 
+                return cb(null, false); 
+            }
+
             if (user)
             {
                 return cb(null, user);
