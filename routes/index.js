6a7162205637a958a4cbbd8e91349ba2a6a0cd1f
jpann <ikonmobile@gmail.com>
2017-05-10 14:51:37 -0600

    Commit refactoring of server for passport path.

---
+++ routes/index.js
@@ -0,0 +1,127 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+var database = require('./../database');
+
+/* GET home page. */
+router.get('/',
+    loggedIn,
+    function(req, res, next)
+    {
+        res.render('home', { "user": req.user });
+    });
+
+router.get('/login',
+    function(req, res)
+    {
+        var e = req.flash('loginMessage')
+        res.render('login', { errors :  e});
+    });
+
+router.post('/login',
+    passport.authenticate('local',
+        {
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        }));
+
+router.get('/logout',
+    function(req, res)
+    {
+        req.logout();
+        res.redirect('/');
+    });
+
+router.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res)
+    {
+        res.render('profile', { "user": req.user });
+    });
+
+router.get('/profile/:id',
+    require('connect-ensure-login').ensureLoggedIn(),
+    checkRole('admin'),
+    function(req, res)
+    {
+        var user_id = req.params.id;
+
+        try
+        {
+            database.getUserById(user_id, function(err, user)
+            {
+                if (!err && user)
+                {
+                    res.render('editUser', { "edit_user": user, "user": req.user });
+                }
+                else
+                {
+                    res.redirect('/listUsers', { "user": req.user });
+                }
+            });
+        }
+        catch (err)
+        {
+            console.log(err);
+
+            res.status(500).send('Error')
+            next(err);
+        }
+    });
+
+router.get('/users',
+    require('connect-ensure-login').ensureLoggedIn(),
+    checkRole('admin'),
+    function(req, res, next)
+    {
+        try
+        {
+            res.render('listUsers', { user: req.user });
+        }
+        catch (err)
+        {
+            next(err);
+        }
+    });
+
+router.get('/clients',
+    require('connect-ensure-login').ensureLoggedIn(),
+    checkRole('admin'),
+    function(req, res, next)
+    {
+        try
+        {
+            res.render('clientsList', { user: req.user });
+        }
+        catch (err)
+        {
+            next(err);
+        }
+    });
+
+module.exports = router;
+
+function checkRole(role)
+{
+    return function(req, res, next)
+    {
+        if (req.user && req.user.role == role)
+            next();
+        else
+            res.send(401, 'Unauthorized');
+    };
+};
+
+function loggedIn(req, res, next) 
+{
+    if (req.user) 
+    {
+        next();
+    }
+    else 
+    {
+        res.redirect('/login');
+    }
+}
